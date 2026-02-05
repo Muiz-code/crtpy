ec19227a396e09abf0b37167789f167cf157ec57
muiz owolabi <beasteye.official@gmail.com>
2026-02-05 22:40:40 +0100

    Fixed

--- app/(contentNav)/aboutUs/milestones/page.tsx
+++ app/(contentNav)/aboutUs/milestones/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useRef } from "react";
 import SectionIndicator from "../../_components/SectionIndicator";
 import { motion } from "framer-motion";
